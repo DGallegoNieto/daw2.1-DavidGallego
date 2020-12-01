70fc585b278aae23bc6d905838e8d53fb297c550
David Gallego <dgallegonieto@educa.madrid.org>
2020-12-02 00:11:01 +0100

    MiniFb

---
+++ 040-miniFb/SesionInicioMostrarFormulario.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+
+
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+</head>
+
+
+
+<body>
+
+<h1>Iniciar Sesión</h1>
+
+<form action='SesionInicioComprobar.php' method='get'>
+    <label>Usuario: </label><input type="text" name="identificador" >
+    <br />
+    <label>Contraseña: </label><input type="text" name="contrasenna">
+    <br />
+    <input type="submit" value="Confirmar">
+</form>
+
+
+</body>
+
+</html>
